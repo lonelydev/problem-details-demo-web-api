abec4a8c4f706a1d2b750d77710132c093ca459e
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2021-05-12 10:59:38 +0100

    feat(example): ProblemDetails a Demo in .NET Core
    Add solution and project to repository

---
+++ ProblemDetailsDemoWebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProblemDetailsDemoWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            throw new Exception("An Unhandled Exception");
+            throw new StormyWeatherException("/weatherforecast", 101);
+            var problem = new StormyWeatherProblemDetails
+            {
+                Type = "https://awesome-weather-forecasts.com/we-got-problems/storm-destruction",
+                Title = "Storm destroyed weather sensors.",
+                Detail = "Unable to retrieve latest weather forecast due to loss of hardware.",
+                Instance = "/weather-problem/987645/storm/hardware-loss",
+                LastReportedWindGust = 98,
+            };
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+
+    public class StormyWeatherProblemDetails : ProblemDetails
+    {
+        public int LastReportedWindGust { get; set; }
+    }
+
+    public class ProblemDetailsException : Exception
+    {
+        public string Type { get; set; }
+        public string Detail { get; set; }
+        public string Title { get; set; }
+        public string Instance { get; set; }
+    }
+
+    public class StormyWeatherException : ProblemDetailsException
+    {
+        public int LastReportedWindGust { get; set; }
+        public StormyWeatherException(string instance, int lastReportedWindGust)
+        {
+            Type = "https://awesome-weather-forecasts.com/we-got-problems/storm-destruction";
+            Title = "Storm destroyed weather sensors.";
+            Detail = "Unable to retrieve latest weather forecast due to loss of hardware.";
+            //Instance = "/weather-problem/987645/storm/hardware-loss";
+            LastReportedWindGust = lastReportedWindGust;
+            Instance = instance;
+        }
+    }
+}
---
+++ ProblemDetailsDemoWebApi/Startup.cs
@@ -0,0 +1,68 @@
+using Hellang.Middleware.ProblemDetails;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ProblemDetailsDemoWebApi.Controllers;
+
+namespace ProblemDetailsDemoWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddProblemDetails(opts =>
+            {
+                // Control when an exception is included
+                opts.IncludeExceptionDetails = (ctx, ex) =>
+                {
+                    // Fetch services from HttpContext.RequestServices
+                    var env = ctx.RequestServices.GetRequiredService<IHostEnvironment>();
+                    return env.IsDevelopment() || env.IsStaging();
+                };
+
+                opts.Map<StormyWeatherException>((swe) => new StormyWeatherProblemDetails()
+                {
+                    Type = swe.Type,
+                    Title = swe.Title,
+                    Detail = swe.Detail,
+                    Instance = swe.Instance,
+                    LastReportedWindGust = swe.LastReportedWindGust,
+                    Status = StatusCodes.Status500InternalServerError
+                });
+            });
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseProblemDetails();
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
